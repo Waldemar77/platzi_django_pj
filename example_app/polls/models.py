28a9679d1eec85e93c2b9a6457704462d552e6a1
Sergio Garzón <sergio.garzon.mar@gmail.com>
2023-02-01 05:49:18 -0500

    Applying ORM concepts to create our model

--- example_app/polls/models.py
+++ example_app/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+
+    # Attributes of this class are the columns of the a table
+    # Id column it isn't necessary wrote because Django sets it automatically
+    question_test = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    
+    # For foreign key, we need to specify the class wherein the primary key is.
+    # "on_delete=models.CASCADE" is used to delete another answers that doesn't match with key.
+    question =models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
