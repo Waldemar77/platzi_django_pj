121f94bb8d391b39c628d28a0ce487efca933675
Sergio Garzón <sergio.garzon.mar@gmail.com>
2023-02-02 21:55:42 -0500

    Aggregate functionality to the views and creating the templates for each view

--- example_app/polls/urls.py
+++ example_app/polls/urls.py
@@ -3,6 +3,19 @@
 from django.urls import path, include
 from . import views
 
+# Variable with the main name of our url
+app_name = 'polls'
+
 urlpatterns = [
+    #example: polls/
     path('', views.index, name='index'),
+    
+    #example: polls/1
+    path('<int:id_question>/detail', views.number_question, name='number_question'),
+
+    #example: polls/1/results
+    path('<int:id_question>/results', views.results, name='question_results'),
+
+    #example: polls/1/votes
+    path('<int:id_question>/votes', views.votes, name='question_votes'),
 ]
--- example_app/polls/views.py
+++ example_app/polls/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('Hello my friend!')
+
+    # In this variable, we get all questions from model object Question. 
+    latest_question_list = Question.objects.all()
+
+    # We returt the template from the templates/polls/index.html and we indicate the variables we need to use. 
+    return render(request, 'polls/index.html', {
+        "latest_question_list":latest_question_list
+    })
+
+
+def number_question(request, id_question):
+    
+    # We're using the get_object_or_404 function to manage the error http when the consult is not effective.
+    question_1 = get_object_or_404(Question, pk=id_question)
+    return render(request, "polls/detail.html", {
+        "question_1":question_1
+    })
+
+
+def results(request, id_question):
+    return HttpResponse(f'You are watching the results for the number question: {id_question}')
+
+
+def votes(request, id_question):
+    return HttpResponse(f'You are voting for the number question: {id_question}')
