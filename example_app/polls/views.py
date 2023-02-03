7b6771a5213c39c20d5908714f854cd6ccbe8638
Sergio Garzón <sergio.garzon.mar@gmail.com>
2023-02-03 05:45:03 -0500

    Programming the view 'votes' in order to interact with the form created in the detail.html template.

--- example_app/polls/views.py
+++ example_app/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 # Create your views here.
@@ -31,4 +32,30 @@ def results(request, id_question):
 
 
 def votes(request, id_question):
-    return HttpResponse(f'You are voting for the number question: {id_question}')
+    
+    # firstly, we must call a object of Question model
+    question_1 = get_object_or_404(Question, pk=id_question)
+
+    # secondly, we manage the flow of the logic through try-except
+    try:
+        # Here, we use the function request.POST[] to get the data from the forms
+        # according to the choice selected.
+        # The POST[] dictionary call the key 'choice', related with the label name in the form.
+        selected_choice = question_1.question_choices.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # If the user doesn't select some item, we render the same "detail" view but we add the
+        # error_message variable to the view.
+        return render(request, 'polls/detail.html', {
+            "question_1":question_1,
+            "error_message":"You must select some anwser. Please, select one item."
+        })
+    else:
+        # If the user selected one item and the request.POST[] found the id, we can start the counter
+        # in the attribute "votes" from Choice Model and save the result into the model (database).
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        # we'll redirect to the user to 'question_results' view through the function 
+        # reverse(), this one is the same code "% url %" used in the templates, but 
+        # now it's wroten in python 
+        return HttpResponseRedirect(reverse('polls:question_results', args=(question_1.id,)))
